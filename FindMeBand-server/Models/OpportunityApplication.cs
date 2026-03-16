86906709787bdcaa6a40f5374a44a8fb286f84e2
Ivan Bebic <ibebic@hrcloud.corp>
2026-03-16 14:23:34 +0100

    Add OpportunityApplication Model

---
+++ FindMeBand-server/Models/OpportunityApplication.cs
@@ -0,0 +1,13 @@
+﻿namespace FindMeBand_server.Models
+{
+    public class OpportunityApplication
+    {
+        public int Id { get; set; }
+        public int OpportunityId { get; set; }
+        public Opportunity Opportunity { get; set; } = null!;
+        public int ApplicantId { get; set; }
+        public Performer Applicant { get; set; } = null!;
+        public DateTime AppliedAt { get; set; }
+        public string? Message { get; set; }
+    }
+}
